aebc5ba63875043d4b489bad644fb80df309ce52
Leonides2 <leonidesart@hotmail.com>
2024-12-07 11:18:02 -0600

    added enviroment variables

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5178");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
